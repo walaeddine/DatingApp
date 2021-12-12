9f7c28b0ff32e87904a78a942e360d0437430d19
walaeddine <walaeddine.bourahla@hotmail.com>
2021-12-12 12:24:22 +0800

    Building API Walking Skeletin Initial

---
+++ API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,14 @@
+﻿using API.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureSqlContext(this IServiceCollection service, IConfiguration configuration) =>
+            service.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+    }
+}
---
+++ API/Startup.cs
@@ -0,0 +1,47 @@
+using API.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace API;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.ConfigureSqlContext(_configuration);
+        services.AddControllers();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+        });
+    }
+
+    public void Configure(IApplicationBuilder applicationBuilder, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            applicationBuilder.UseDeveloperExceptionPage();
+            applicationBuilder.UseSwagger();
+            applicationBuilder.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+        }
+
+        applicationBuilder.UseHttpsRedirection();
+        applicationBuilder.UseRouting();
+        applicationBuilder.UseAuthorization();
+        applicationBuilder.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
+
